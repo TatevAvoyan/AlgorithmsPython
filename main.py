d1ab39a23225f8077509cfa69216d2f6a0f20dc3
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-09 15:57:18 +0400

    Insertion Sort

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 data = [5, 2, 7, 4, 1, 3, 6]
 
-def selection_sort(data):
-    marker = 0
-    while marker < len(data):
-        min_index = marker
-        for i in range(marker, len(data)):
-            if data[marker] > data[i]:
-                if data[i] < data[min_index]:
-                    min_index = i
-        data[marker], data[min_index] = data[min_index], data[marker]
+def insertion_sort(data):
+
+    for i in range(1, len(data)):
+        while data[i-1] > data[i] and i > 0:
+            data[i], data[i-1] = data[i-1], data[i]
+            i -= 1
         print(data)
 
-
-        marker+=1
-
-    print(data)
-
-
-
-selection_sort(data)
+insertion_sort(data)
