0db569df6f1ef0533c5d33011814575a17f8b74e
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-09 16:29:30 +0400

    Big O notation in Insertion sort

--- main.py
+++ main.py
@@ -2,24 +2,19 @@ data = [5, 1, 4, 3, 2, 10, 8]
 data_best = [1, 2, 3, 4, 5, 8, 10]
 data_worst = [10, 8, 5, 4, 3, 2, 1]
 
-def selection_sort(data):
-    marker = 0
+def insertion_sort(data):
+
     steps = 0
-    while marker < len(data):
+    for i in range(1, len(data)):
         steps += 1
-        min_index = marker
-        for i in range(marker, len(data)):
+        while data[i-1] > data[i] and i > 0:
             steps += 1
-            if data[marker] > data[i]:
-                if data[i] < data[min_index]:
-                    min_index = i
-        data[marker], data[min_index] = data[min_index], data[marker]
+            data[i], data[i-1] = data[i-1], data[i]
+            i -= 1
         print(data)
-        marker+=1
 
     print(data)
     print(steps)
 
-
-# 35 in data, data_best, data_worst
-selection_sort(data_worst)
+# 14 in data, 6 in data_best, 27 in data_worst
+insertion_sort(data_worst)
