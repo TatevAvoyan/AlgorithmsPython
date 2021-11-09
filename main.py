2ed1ffcd3166a4a7614d39d19a5b32cc7d5af87a
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-09 16:25:52 +0400

    Big O notation in Selection sort

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ data = [5, 1, 4, 3, 2, 10, 8]
 data_best = [1, 2, 3, 4, 5, 8, 10]
 data_worst = [10, 8, 5, 4, 3, 2, 1]
 
-def bubble_sort(data):
+def selection_sort(data):
+    marker = 0
     steps = 0
-    swappend = True
-    while swappend:
+    while marker < len(data):
         steps += 1
-        swappend = False
-        for i in range(len(data)-1):
+        min_index = marker
+        for i in range(marker, len(data)):
             steps += 1
-            if data[i] > data[i+1]:
-                data[i], data[i+1] = data[i+1], data[i]
-                swappend = True
-                print(data)
+            if data[marker] > data[i]:
+                if data[i] < data[min_index]:
+                    min_index = i
+        data[marker], data[min_index] = data[min_index], data[marker]
+        print(data)
+        marker+=1
+
     print(data)
     print(steps)
 
 
-bubble_sort(data_worst)
+# 35 in data, data_best, data_worst
+selection_sort(data_worst)
