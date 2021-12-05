0baed53d249b8bf9fd163130b344ecce26e71b35
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-12-05 22:24:41 +0400

    Bubble sort

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-data = [5, 1, 4, 3, 2, 10, 8]
+data1 = [5, 1, 4, 3, 2, 10, 8]
 
-def bubble_sort(data):
+
+def bubble_sort(data0):
+
     swappend = True
+
     while swappend:
         swappend = False
-        for i in range(len(data)-1):
-            if data[i] > data[i+1]:
-                data[i], data[i+1] = data[i+1], data[i]
+        for i in range(len(data0)-1):
+            if data0[i] > data0[i+1]:
+                data0[i], data0[i+1] = data0[i+1], data0[i]
                 swappend = True
-                print(data)
-    print(data)
+                print(data0)
+    print(data0)
 
 
-bubble_sort(data)
+bubble_sort(data1)
